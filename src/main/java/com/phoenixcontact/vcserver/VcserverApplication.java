f157b220db5015d93b6e46ee8c0e13117f04bb4e
Unknown <mikelgmutio@gmail.com>
2020-11-29 13:44:34 +0100

    Springboot project created

---
+++ src/main/java/com/phoenixcontact/vcserver/VcserverApplication.java
@@ -0,0 +1,13 @@
+package com.phoenixcontact.vcserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VcserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VcserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phoenixcontact/vcserver/VcserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phoenixcontact.vcserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VcserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
